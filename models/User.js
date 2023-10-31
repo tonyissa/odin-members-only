970278fd54d8c7516ae4376c75587d1002007882
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-10-31 18:08:47 -0400

    Update user model
    Add virtual property for full name

--- models/User.js
+++ models/User.js
@@ -3,7 +3,14 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    membership: { type: String, enum: ["Unregistered", "Member", "Admin"] }
 });
+
+userSchema.virtual('fullName').get(function() {
+    return `${this.firstName} ${this.lastName}`;
+})
 
 module.exports = mongoose.model("User", userSchema);
