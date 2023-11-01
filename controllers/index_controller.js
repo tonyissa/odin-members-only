f23548d38caa6de32da03a32c1d4032ab0f112cd
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-01 17:58:14 -0400

    Add rendering of all views in controllers

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -8,22 +8,20 @@ exports.index_get = asyncHandler(async(req, res, next) => {
 })
 
 exports.sign_up_get = asyncHandler(async(req, res, next) => {
-    
-
+    res.render('pages/sign-up');
 })
 
 exports.sign_in_get = asyncHandler(async(req, res, next) => {
-    
-
+    res.render('pages/sign-in');
 })
 
 // POST
 exports.sign_up_post = asyncHandler(async(req, res, next) => {
     
-
+    res.redirect('/')
 })
 
 exports.sign_in_post = asyncHandler(async(req, res, next) => {
     
-
+    res.redirect('/')
 })
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,26 +5,25 @@ const asyncHandler = require("express-async-handler");
 
 // GET
 exports.upgrade_account_get = asyncHandler(async(req, res, next) => {
-
+    res.render('pages/upgrade-account');
 })
 
 exports.create_story_get = asyncHandler(async(req, res, next) => {
-    
-
+    res.render('pages/create-story');
 })
 
 exports.log_out_get = asyncHandler(async(req, res, next) => {
-    
 
+    res.redirect('/')
 })
 
 // POST
 exports.upgrade_account_post = asyncHandler(async(req, res, next) => {
 
-
+    res.redirect('/')
 })
 
 exports.create_story_post = asyncHandler(async(req, res, next) => {
     
-
+    res.redirect('/')
 })
