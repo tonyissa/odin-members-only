56e9204f9ab2cb68607f8eea49d73c107f2daca1
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-01 14:02:46 -0400

    Finish boilerplate code of user routes and controllers

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,59 @@ const Story = require('../models/Story');
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
+// GET
+exports.become_member_get = asyncHandler(async(req, res, next) => {
+
+
+})
+
+exports.become_admin_get = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+exports.create_story_get = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+exports.sign_in_get = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+exports.sign_up_get = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+exports.log_out_get = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+// POST
+exports.become_member_post = asyncHandler(async(req, res, next) => {
+
+
+})
+
+exports.become_admin_post = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+exports.create_story_post = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+exports.sign_in_post = asyncHandler(async(req, res, next) => {
+    
+
+})
+
+exports.sign_up_post = asyncHandler(async(req, res, next) => {
+    
+
+})
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,28 @@ var router = express.Router();
 
 const user_controller = require('../controllers/user_controller');
 
+// GET
+router.get('/become-member', user_controller.become_member_get);
 
+router.get('/become-admin', user_controller.become_admin_get);
+
+router.get('/create-story', user_controller.create_story_get);
+
+router.get('/sign-in', user_controller.sign_in_get);
+
+router.get('/sign-up', user_controller.sign_up_get);
+
+router.get('/log-out', user_controller.log_out_get);
+
+// POST
+router.post('/become-member', user_controller.become_member_post);
+
+router.post('/become-admin', user_controller.become_admin_post);
+
+router.post('/create-story', user_controller.create_story_post);
+
+router.post('/sign-in', user_controller.sign_in_post);
+
+router.post('/sign-up', user_controller.sign_up_post);
 
 module.exports = router;
