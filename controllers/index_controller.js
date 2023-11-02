f12e4ab2e3cfca9891fbaec84ae089515945eead
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-11-02 19:30:21 -0400

    Add validation and error rendering of sign-up page

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -2,6 +2,8 @@ const Story = require('../models/Story');
 
 const asyncHandler = require("express-async-handler");
 
+const { body, validationResult } = require("express-validator");
+
 // GET
 exports.index_get = asyncHandler(async(req, res, next) => {
     res.render('pages/index');
@@ -16,10 +18,24 @@ exports.sign_in_get = asyncHandler(async(req, res, next) => {
 })
 
 // POST
-exports.sign_up_post = asyncHandler(async(req, res, next) => {
-    
-    res.redirect('/')
-})
+exports.sign_up_post = [
+    body('email').trim().notEmpty().withMessage('Email is required').isEmail().withMessage('Email is not valid').escape(),
+    body('password').trim().notEmpty().withMessage('Password is required').escape(),
+    body('confirmPassword').trim().notEmpty().escape().custom((value, {req}) => {
+        if (value !== req.body.password) { 
+            throw new Error('Passwords do not match')
+        }
+        return true;
+    }),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render('pages/sign-up', { errors: errors.array() });
+        } else {
+
+        }
+    })
+]
 
 exports.sign_in_post = asyncHandler(async(req, res, next) => {
     
